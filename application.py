17e53a1de0856649466a4dc028610e2358545b96
Derrick Njobuenwu <donadviser@gmail.com>
2024-05-24 12:04:54 +0100

    update deployment

--- application.py
+++ application.py
@@ -1,38 +1,47 @@
-from flask import Flask, request, render_template,jsonify
-from flask_cors import CORS,cross_origin
-from pipelines.inference_pipeline import CustomData, PredictPipeline
+from flask import Flask,request,render_template
+import numpy as np
+import pandas as pd
 
-application = Flask(__name__)
+from sklearn.preprocessing import StandardScaler
+from pipelines.inference_pipeline import CustomData,PredictPipeline
 
-@application.route('/')
-@cross_origin()
-def home_page():
-    return render_template('index.html')
+application=Flask(__name__)
 
-@application.route('/predict',methods=['GET','POST'])
-@cross_origin()
+app=application
+
+## Route for a home page
+
+@app.route('/')
+def index():
+    return render_template('index.html') 
+
+@app.route('/predictdata',methods=['GET','POST'])
 def predict_datapoint():
-    if request.method == 'GET':
-        return render_template('index.html')
+    if request.method=='GET':
+        return render_template('home.html')
     else:
-        data = CustomData(
-            gender = request.form.get('gender'),
-            race_ethnicity = request.form.get('ethnicity'),
-            parental_level_of_education = request.form.get('parental_level_of_education'),
-            lunch = request.form.get('lunch'),
-            test_preparation_course = request.form.get('test_preparation_course'),
-            reading_score = float(request.form.get('reading_score')),
-            writing_score = float(request.form.get('writing_score'))
+        data=CustomData(
+            gender=request.form.get('gender'),
+            race_ethnicity=request.form.get('ethnicity'),
+            parental_level_of_education=request.form.get('parental_level_of_education'),
+            lunch=request.form.get('lunch'),
+            test_preparation_course=request.form.get('test_preparation_course'),
+            reading_score=float(request.form.get('writing_score')),
+            writing_score=float(request.form.get('reading_score'))
+
         )
-
-        pred_df = data.prepare_data_for_inference()
-        
+        pred_df=data.prepare_data_for_inference()
         print(pred_df)
+        print("Before Prediction")
 
-        predict_pipeline = PredictPipeline()
-        pred = predict_pipeline.predict(pred_df)
-        results = round(pred[0],2)
-        return render_template('index.html',results=results)
+        predict_pipeline=PredictPipeline()
+        print("Mid Prediction")
+        results=predict_pipeline.predict(pred_df)
+        print("after Prediction")
+        return render_template('home.html',results=results[0])
     
-if __name__ == '__main__':
-    application.run(host='0.0.0.0')
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0")        
+
+
